6849b6185c5849ce5953fe7fdaa9159f36beca3e
hjlee5701 <hjlee5701@gmail.com>
2025-02-07 22:09:42 +0900

    Initial commit

---
+++ src/main/java/com/hanghae/prevstudy/PrevStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.prevstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrevStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/prevstudy/PrevStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.prevstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
