074bb0d9e1591c352c3f723fbd72a7dbf86cb1ef
hjlee5701 <hjlee5701@gmail.com>
2025-02-08 00:34:33 +0900

    test: BoardRepository 게시글 저장 테스트 성공
    - Board entity 의 컬럼을 추가한다.
    - 게시글 저장 성공 여부를 확인한다. -> isEqualTo

--- src/test/java/com/hanghae/prevstudy/domain/board/BoardRepositoryTest.java
+++ src/test/java/com/hanghae/prevstudy/domain/board/BoardRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.hanghae.prevstudy.domain.board;
 
-import com.hanghae.prevstudy.board.BoardRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,6 +20,14 @@ public class BoardRepositoryTest {
     @DisplayName("BoardRepository_널_체크")
     void BoardRepository_isNull() {
         assertThat(boardRepository).isNotNull();
+    }
+
+    @Test
+    @DisplayName("게시글_저장")
+    void 게시글_저장() {
+        Board board = new Board("제목", "작성자", "내용", "비밀번호");
+        Board newBoard = boardRepository.save(board);
+        assertThat(board).isEqualTo(newBoard);
     }
 }
 
