c04b1ecb1f15f65c89061c31359010ce5da93bfc
hjlee5701 <hjlee5701@gmail.com>
2025-02-09 00:03:21 +0900

    test: BoardController 게시글 저장 테스트 실패
    - BoardControllerTest 생성
    - BoardController.addBoard 메소드 생성
    - RequestBody, ResponseBody 파일 미생성으로 실패

--- src/main/java/com/hanghae/prevstudy/domain/board/BoardController.java
+++ src/main/java/com/hanghae/prevstudy/domain/board/BoardController.java
@@ -1,7 +1,23 @@
 package com.hanghae.prevstudy.domain.board;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity<BoardAddResponse> addBoard(@RequestBody BoardAddRequest boardAddRequest) {
+        return new ResponseEntity<>(boardService.add(boardAddRequest), HttpStatus.OK);
+
+    }
 }
--- src/test/java/com/hanghae/prevstudy/domain/board/BoardControllerTest.java
+++ src/test/java/com/hanghae/prevstudy/domain/board/BoardControllerTest.java
@@ -1,18 +1,27 @@
 package com.hanghae.prevstudy.domain.board;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardControllerTest {
+    private static final String REQUEST_URL = "/board";
 
     @InjectMocks
     private BoardController boardController;
@@ -31,4 +40,31 @@ public class BoardControllerTest {
         assertThat(boardController).isNotNull();
         assertThat(mockMvc).isNotNull();
     }
+
+    private String createAddRequestJson() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            BoardAddRequest boardAddRequest = new BoardAddRequest(
+                    "제목", "작성자", "내용", "비밀번호"
+            );
+            return objectMapper.writeValueAsString(boardAddRequest);
+        } catch (JsonProcessingException e) {
+            return "{}";
+        }
+    }
+
+    @Test
+    @DisplayName("게시글_생성")
+    void 게시글_생성() throws Exception {
+        BoardAddResponse boardAddResponse = BoardAddResponse.builder().build();
+        doReturn(boardAddResponse).when(boardController).addBoard(any(BoardAddRequest.class));
+        final ResultActions addResult = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post(REQUEST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(createAddRequestJson())
+        );
+        addResult.andExpect(status().is2xxSuccessful());
+    }
+
 }
