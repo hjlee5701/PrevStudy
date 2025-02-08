758d6c8d9ff15f073d0167fcefa4502ad315d38f
hjlee5701 <hjlee5701@gmail.com>
2025-02-09 01:34:10 +0900

    test: BoardRepository 게시글 상세 조회 테스트 성공

--- src/test/java/com/hanghae/prevstudy/domain/board/BoardRepositoryTest.java
+++ src/test/java/com/hanghae/prevstudy/domain/board/BoardRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -33,6 +35,27 @@ public class BoardRepositoryTest {
                 .build();
         Board newBoard = boardRepository.save(board);
         assertThat(board).isEqualTo(newBoard);
+    }
+
+    @Test
+    @DisplayName("게시글_상세_조회")
+    void 게시글_상세_조회() {
+        // given
+        Board board = Board.builder()
+                .title("제목")
+                .writer("작성자")
+                .content("내용")
+                .password("비밀번호")
+                .build();
+        Board savedBoard = boardRepository.save(board);
+
+        // when
+        Optional<Board> findBoard = boardRepository.findById(1L);
+
+        // then
+        assertThat(findBoard).isNotNull();
+        assertThat(findBoard.get().getId()).isEqualTo(1L);
+        assertThat(findBoard.get().getContent()).isEqualTo(savedBoard.getContent());
     }
 }
 
