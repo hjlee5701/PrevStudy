72200a9037cb670aa9905b1acf75a498bc4a1ab4
hjlee5701 <hjlee5701@gmail.com>
2025-02-09 18:10:34 +0900

    test: BoardServiceTest 게시글_전체_조회 -> 테스트 성공
    - boardService.getBoards() 구현

--- src/main/java/com/hanghae/prevstudy/domain/board/BoardService.java
+++ src/main/java/com/hanghae/prevstudy/domain/board/BoardService.java
@@ -1,7 +1,11 @@
 package com.hanghae.prevstudy.domain.board;
 
+import java.util.List;
+
 public interface BoardService {
     BoardResponse add(BoardAddRequest board);
 
     BoardResponse getBoard(Long boardId);
+
+    List<BoardResponse> getBoards();
 }
