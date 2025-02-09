17d15f5e7f82ad9a2f6e2c2484f7b544cda3643e
hjlee5701 <hjlee5701@gmail.com>
2025-02-09 17:26:46 +0900

    test: BoardControllerTest 게시글_상세_조회_실패 -> 테스트 성공
    - BoardController.getBoard 구현
    - GlobalException 에서 전역 커스텀 예외 핸들링
    - 커스텀 에러 발생시 HttpStatusCode 설정 및 반환 기능 추가

--- src/main/java/com/hanghae/prevstudy/global/exception/ErrorCode.java
+++ src/main/java/com/hanghae/prevstudy/global/exception/ErrorCode.java
@@ -6,4 +6,6 @@ public interface ErrorCode {
     String getErrCode();
     String getMessage();
 
+    HttpStatus getStatus();
+
 }
--- src/main/java/com/hanghae/prevstudy/global/exception/PrevStudyException.java
+++ src/main/java/com/hanghae/prevstudy/global/exception/PrevStudyException.java
@@ -1,16 +1,19 @@
 package com.hanghae.prevstudy.global.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class PrevStudyException extends RuntimeException {
     private final String errCode;
     private final String message;
+    private final HttpStatus status;
 
     public PrevStudyException(BoardErrorCode boardErrorCode) {
         super(boardErrorCode.getMessage());
         this.errCode = boardErrorCode.getErrCode();
         this.message = boardErrorCode.getMessage();
+        this.status = boardErrorCode.getStatus();
     }
 
 }
