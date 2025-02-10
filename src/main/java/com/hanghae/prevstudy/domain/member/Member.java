f3e15ce9dcce54b006e982fffcea5c99c3f40f7b
hjlee5701 <hjlee5701@gmail.com>
2025-02-10 22:59:12 +0900

    test: MemberRepository 회원_가입_username_중복 -> 테스트 성공
    - username, password 컬럼 추가
    - username 제약조건 (unique)

--- src/main/java/com/hanghae/prevstudy/domain/member/Member.java
+++ src/main/java/com/hanghae/prevstudy/domain/member/Member.java
@@ -4,10 +4,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 @Entity
 public class Member {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
 }
--- src/test/java/com/hanghae/prevstudy/domain/member/MemberRepositoryTest.java
+++ src/test/java/com/hanghae/prevstudy/domain/member/MemberRepositoryTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -19,6 +22,29 @@ public class MemberRepositoryTest {
     @DisplayName("MemberRepository_널_체크")
     void MemberRepository_isNull() {
         assertThat(memberRepository).isNotNull();
+    }
+
+    @Test
+    @DisplayName("회원_가입_username_중복")
+    void 회원_가입_username_중복() {
+        // given
+        Member member1 = Member.builder()
+                .username("회원")
+                .password("비밀번호")
+                .build();
+        memberRepository.save(member1);
+        memberRepository.flush();
+
+        // when
+        Member member2 = Member.builder()
+                .username("회원")
+                .password("비밀번호")
+                .build();
+
+        memberRepository.save(member2);
+
+        // then
+        assertThrows(DataAccessException.class, () -> memberRepository.flush());
     }
 
 
